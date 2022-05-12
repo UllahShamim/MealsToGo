a3d81515a895e64efd3dc8d65037041741704456
Shamim Ullah <shamim.ullah.86@gmail.com>
2022-05-12 17:55:38 +0100

    started stacked navigation

---
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screens";
+
+const RestaurantStack = createStackNavigator();
+
+export const RestaurantsNavigator = () => {
+  return (
+    <RestaurantStack.Navigator>
+      <RestaurantStack.Screen
+        name="Restaurants"
+        component={RestaurantsScreen}
+      />
+    </RestaurantStack.Navigator>
+  );
+};
